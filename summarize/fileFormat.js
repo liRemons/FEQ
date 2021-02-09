46fb5ed4f7a754dd5f84094c40ac598b6dbf0970
aaa <123@qq.com>
2021-02-09 17:59:55 +0800

    udpate

--- summarize/fileFormat.js
+++ summarize/fileFormat.js
@@ -13,7 +13,7 @@ fs.readdir("./html", (err, files) => {
 const readFile = (path, name) => {
   let data = fs
     .readFileSync(path, "utf-8")
-    .replace(/<style.*?>([\s\S]*)<\/style>/, "");
+    .replace(/<style.*?>([\s\S]*)<\/style>/, "").replace("<p><a href='http://remons.gitee.io/'><span>个人网站持续更新</span></a></p>", '');
   writeFile(data, name);
 };
 
