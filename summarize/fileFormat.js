786b69cf72307c43cd62dfbf6a27bef3361f0519
aaa <123@qq.com>
2021-02-18 18:08:49 +0800

    udpate nocache

--- summarize/fileFormat.js
+++ summarize/fileFormat.js
@@ -13,17 +13,7 @@ fs.readdir("./html", (err, files) => {
 const readFile = (path, name) => {
   let data = fs
     .readFileSync(path, "utf-8")
-    .replace(/<style.*?>([\s\S]*)<\/style>/, "")
-    // 设置页面不适用缓存
-    .replace("<meta charset='UTF-8'>", 
-   `<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=8">
-    <meta http-equiv="Expires" content="0">
-    <meta http-equiv="Pragma" content="no-cache">
-    <meta http-equiv="Cache-control" content="no-cache">
-    <meta http-equiv="Cache" content="no-cache">
-  `)
-    .replace("<p><a href='http://remons.gitee.io/'><span>个人网站持续更新</span></a></p>", '');
+    .replace(/<style.*?>([\s\S]*)<\/style>/, "").replace("<p><a href='http://remons.gitee.io/'><span>个人网站持续更新</span></a></p>", '');
   writeFile(data, name);
 };
 
