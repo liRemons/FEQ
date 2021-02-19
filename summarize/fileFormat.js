e149324be9bab4dd445ae7c5d44cc9c248928acc
aaa <123@qq.com>
2021-02-19 16:44:53 +0800

    setting test nocache

--- summarize/fileFormat.js
+++ summarize/fileFormat.js
@@ -13,8 +13,14 @@ fs.readdir("./html", (err, files) => {
 const readFile = (path, name) => {
   let data = fs
     .readFileSync(path, "utf-8")
-    .replace(/<style.*?>([\s\S]*)<\/style>/, "").replace("<p><a href='http://remons.gitee.io/'><span>个人网站持续更新</span></a></p>", '');
+    .replace(/<style.*?>([\s\S]*)<\/style>/, "")
+    .replace("<meta charset='UTF-8'>", `<meta charset='UTF-8'><meta http-equiv="Expires" content="0">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Cache-control" content="no-cache">
+    <meta http-equiv="Cache" content="no-cache">`)
+    .replace("<p><a href='http://remons.gitee.io/'><span>个人网站持续更新</span></a></p>", '');
   writeFile(data, name);
+
 };
 
 const writeFile = (data, name) => {
