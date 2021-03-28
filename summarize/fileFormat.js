f91472dacf6ec2a7e30b06822276970b8fd6679e
liRemons <remons@foxmail.com>
2021-03-28 18:47:52 +0800

    新增doc

--- summarize/fileFormat.js
+++ summarize/fileFormat.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: Remons
+ * @Date: 2021-02-11 16:32:56
+ * @LastEditors: Remons
+ * @LastEditTime: 2021-03-28 18:46:33
+ */
 // 格式化html，去除style
 const fs = require("fs");
 fs.readdir("./html", (err, files) => {
@@ -10,16 +18,26 @@ fs.readdir("./html", (err, files) => {
   }
 });
 
+fs.readdir("./website/assets/documents/", (err, files) => {
+  const path = "./website/data/";
+  const name = "documentsNameList.json";
+  writeFile({ path, name, data: JSON.stringify(files) });
+});
+
 const readFile = (path, name) => {
   let regExp = /<body[^>]*>([\s\S]+?)<\/body>/i;
   let data = fs
     .readFileSync(path, "utf-8")
     // .replace(/<style.*?>([\s\S]*)<\/style>/, "")
-    .replace("<p><a href='http://remons.gitee.io/'><span>个人网站持续更新</span></a></p>", '');
-  let m = regExp.exec(data);
-  m && writeFile(m[1], name);
+    .replace(
+      "<p><a href='http://remons.gitee.io/'><span>个人网站持续更新</span></a></p>",
+      ""
+    );
+  const m = regExp.exec(data);
+  m && writeFile({ data: m[1], name, path: "./html/" });
 };
 
-const writeFile = (data, name) => {
-  fs.writeFile("./html/" + name, data, "utf-8", (err) => { });
+const writeFile = ({ data, name, path }) => {
+  console.log(data, "data");
+  fs.writeFile(path + name, data, "utf-8", (err) => {});
 };
